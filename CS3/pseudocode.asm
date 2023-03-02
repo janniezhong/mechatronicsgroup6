89cf7cfe867de1460fa131bcc7b68d6399ea6156
Jannie Zhong <24399232+janniezhong@users.noreply.github.com>
2023-03-02 18:47:10 -0500

    start pseudocode for cs3

---
+++ CS3/pseudocode.asm
@@ -0,0 +1,31 @@
+; ** init: initalize ports, registers, determine output/inputs
+; variables: mode, val in A/D converter, count (for modes 2 and 4), state(?)
+; port B 0-3: indicator LEDs
+; port E: octal switch
+
+
+; ** after init, waiting for mode:
+; check for green press
+; if no, check for green press
+; if yes, check for green release
+    ; if no check for green release
+    ; if yes read the mode switch (needs to be complemented, shows 001 for 6)
+
+
+; ** switch mode:
+; display mode on the LEDS, turn off the transistors
+; go to mode (1-4)
+
+
+; ** mode 1
+
+; ** mode 2
+
+; ** mode 3
+
+; ** mode 4
+
+
+; ** faults: if fault triggered, turn the fault LED on, then wait for the black reset switch
+
+; ** if reset, jump to init
